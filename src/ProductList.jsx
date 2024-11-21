c5522ab39f42649e711531a4d0891c3298f503a8
rjmax13 <rod_max@sbcglobal.net>
2024-11-21 10:55:56 -0500

    add to cart functionality

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -1,9 +1,11 @@
 import React, { useState,useEffect } from 'react';
 import './ProductList.css'
 import CartItem from './CartItem';
+import { addItem } from './CartSlice';
 function ProductList() {
     const [showCart, setShowCart] = useState(false); 
     const [showPlants, setShowPlants] = useState(false); // State to control the visibility of the About Us page
+    const [addedToCart, setAddedToCart] = useState({}); // State to track which products are added to the cart
 
     const plantsArray = [
         {
@@ -246,6 +248,17 @@ const handlePlantsClick = (e) => {
     e.preventDefault();
     setShowCart(false);
   };
+
+  const handleAddToCart = (product) =>{
+    dispatchEvent(addItem(product))
+    setAddedToCart((prevState) => ({
+        ...prevState,
+        [product.name]: true
+    }))
+
+  }
+
+
     return (
         <div>
              <div className="navbar" style={styleObj}>
